bf86ce41a4d59d73d551989a138a7a8e823ebc91
David Fisher <tibbon@gmail.com>
2015-02-27 14:33:59 -0500

    Displaying filenames at top level on screen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,42 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+var App = (function() {
+  var $filesDiv;
+  var init = function() {
+    $filesDiv = $('#fileBrowser');
+    loadFiles();
+  };
+
+  var loadFiles = function() {
+    $.ajax({
+      url: 'http://localhost:3000/fs_items',
+      type: 'GET',
+      dataType: 'json'
+    })
+    .done(function(files) {
+      renderFiles(files.fs_items);
+    })
+    .fail(function(err) {
+      console.log('error' + err);
+    });
+  };
+
+  var renderFiles = function(files) {
+    for(var i = 0; i < files.length; i++) {
+      renderFile(files[i]);
+    }
+  };
+
+  var renderFile = function(file) {
+    var $fileDiv = $('<div>');
+    // var $fileLink = $('<a>');
+    $fileDiv.text(file.name);
+    $filesDiv.append($fileDiv);
+  };
+
+  return {init: init};
+})();
+
+$(document).ready(function() {
+  App.init();
+});
